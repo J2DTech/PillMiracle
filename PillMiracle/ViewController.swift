a81854762d7be41a15dfa8f2bcbf0b0b17726997
J2DTech <diazdiaz.joel2@gmail.com>
2017-01-27 20:31:31 -0500

    Initial Commit

---
+++ PillMiracle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PillMiracle
+//
+//  Created by Glorymar Figueroa on 1/27/17.
+//  Copyright © 2017 Joel Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
